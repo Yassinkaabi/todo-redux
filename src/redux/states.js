848d7f8059fa6a38f4bf8fef91937bbfc32f7129
yassine <yassinkaabi14@gmail.com>
2022-01-04 20:43:22 +0100

    first commit

---
+++ src/redux/states.js
@@ -0,0 +1,18 @@
+export let todos = [
+    {
+        id:1,
+       name: "yassin",
+        done:false
+
+    },
+    {
+        id:2,
+        name: "amine",
+        done:false
+    },
+    {
+        id:3,
+       name: "dhia",
+        done:false 
+    },
+]
